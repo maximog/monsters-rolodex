702d146dd038c06cca031b239bbcbcab252c6ab5
maximog <maximo.gijon@gmail.com>
2020-02-03 16:16:24 -0300

    Updates

---
+++ precache-manifest.5adcfe93379b49333472762b8f6787da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4180ffac8f7b66304c17c092534073ef",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9a5ca998d89c13fc9b34",
+    "url": "/monsters-rolodex/static/css/main.d8a0bb07.chunk.css"
+  },
+  {
+    "revision": "fb640c9581c4bf6d0dcc",
+    "url": "/monsters-rolodex/static/js/2.f6b4a9cb.chunk.js"
+  },
+  {
+    "revision": "9a5ca998d89c13fc9b34",
+    "url": "/monsters-rolodex/static/js/main.f54adb1c.chunk.js"
+  },
+  {
+    "revision": "8c5c3c32a70e80a01b13",
+    "url": "/monsters-rolodex/static/js/runtime-main.1f48cb5c.js"
+  }
+]);
